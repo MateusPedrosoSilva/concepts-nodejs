5b28c6f48e49b88370e1c512be3304f278a55f17
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2020-06-17 00:27:10 -0300

    Made post, likes and delete

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,11 +10,19 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,url,techs} = request.body;
+  const repository = {
+    id:uuid(),
+    title,
+    url,
+    techs,
+    likes:0
+  }
+  return response.status(200).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -23,11 +30,23 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  if(!repositoryIndex){
+    return response.status(400).json({error:'Repositry do not exist'});
+  }
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repository = repositories.find(repository => repository.id == id);
+  if(!repository){
+    return response.status(400).json({error:'repository not found.'});
+  }
+  repository.likes++;
+  return reponse.status(200).json(repository);
 });
 
 module.exports = app;
